09fb6ed0c424f9a15fd6749151c1ea5c7e367652
Anshul Gupta <anshul@grappus.com>
2017-11-14 14:39:25 +0530

    python2 compatiable

--- AwsS3Manager.py
+++ AwsS3Manager.py
@@ -8,11 +8,10 @@ from boto3.session import Session
 
 
 class AwsS3Manager(object):
-	__AWS_KEY = "your aws key"
-	__AWS_SECRET = "your aws secret"
+	__AWS_KEY = ""
+	__AWS_SECRET = ""
 
 	def __init__(self, bucket_name):
-		print(" AwsS3Manager Initilized.")
 		self.__bucket_name = bucket_name
 		self.__session = Session(aws_access_key_id=self.__AWS_KEY,
                   				 aws_secret_access_key=self.__AWS_SECRET,
@@ -21,7 +20,7 @@ class AwsS3Manager(object):
 
 	def upload_file(self,file_name,filepath):
 		data = open(filepath, 'rb')
-		object_ = self.s3.Bucket(self.__bucket_name).put_object(Key=file_name, Body=data)
+		object_ = self.s3.Bucket(self.__bucket_name).put_object(Key=file_name, Body=data, ACL='public-read')
 		s3client = self.__session.client('s3')
 		url = s3client.generate_presigned_url('put_object', Params={'Bucket': self.__bucket_name, 'Key': object_.key})
 		return {"url":url,"key":object_.key}
